0e9f5e4831531ca37cc922df20fb4e20fa9f8a63
Derald Blesman <dblessman24@gmail.com>
2025-03-19 08:55:35 -0400

    Initial Commit

---
+++ Yogo Watch App/YogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YogoApp.swift
+//  Yogo Watch App
+//
+//  Created by Derald Blessman on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Yogo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
