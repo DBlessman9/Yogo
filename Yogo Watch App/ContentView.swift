01e186992a7455afbaab3ed38000595e013cb9f4
Derald Blesman <dblessman24@gmail.com>
2025-03-30 11:42:08 -0400

    navigation issue on conrtent view (main Page)

--- Yogo Watch App/ContentView.swift
+++ Yogo Watch App/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
                 }
                 .foregroundColor(.tan)
             }
+            .navigationBarBackButtonHidden()
         }
         .environmentObject(audioManager)
         .environmentObject(currentYoga)
